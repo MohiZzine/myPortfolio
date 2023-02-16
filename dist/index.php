204c13f133c355a2e13ccfebcdcfd0bdfc38e9b9
Mohieddine Farid <farid.mohieddine3@gmail.com>
2023-02-16 01:19:49 +0100

    Add author, keywords and description meta tags

--- dist/index.php
+++ dist/index.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="Personal Portfolio, Mohieddine Farid, Computer Science, Web Development, Mathematics Olympiad, Blogging">
+  <meta name="description" content="Homepage of Mohieddine Farid, Course Notes, olympiad materials, writings, CV.">
+  <meta name="author" content="Mohieddine Farid">
   <title>Mohieddine Farid Portfolio</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
